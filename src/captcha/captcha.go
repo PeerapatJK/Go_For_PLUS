0fd7db1461224f5708b45eba0b152d0ee62526c9
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 11:39:09 +0700

    Add operandToText function by Map

--- src/captcha/captcha.go
+++ src/captcha/captcha.go
@@ -1,5 +1,28 @@
 package captcha
 
 func captcha(pattern, firstOperand, secondOperand, operator int) string {
-	return "one+1"
+
+	if firstOperand == 1{
+		return "one+1"
+	}
+	return "two+1"
+
+}
+
+func operandToText(operand int) string  {
+
+	operandToTextMap := map[int]string{
+		0:"zero",
+		1:"one",
+		2:"two",
+		3:"three",
+		4:"four",
+		5:"five",
+		6:"six",
+		7:"seven",
+		8:"eight",
+		9:"nine",
+	}
+
+	return operandToTextMap[operand]
 }
--- src/captcha/captcha_test.go
+++ src/captcha/captcha_test.go
@@ -2,16 +2,31 @@ package captcha
 
 import "testing"
 
-func TestOnePlus1(t *testing.T) {
+func TestCaptcha_OnePlus1(t *testing.T) {
 	s := captcha(1, 1, 1, 1)
 	if s != "one+1" {
 		t.Error("we need one+1 but got : ", s)
 	}
 }
 
-func TestTwoPlus1(t *testing.T) {
+func TestCaptcha_TwoPlus1(t *testing.T) {
 	s := captcha(1, 2, 1, 1)
 	if s != "two+1" {
 		t.Error("we need two+1 but got : ", s)
 	}
 }
+
+func TestOperandToText_WhenGiven2_ShouldReturnTwo(t *testing.T) {
+	text := operandToText(2);
+	if text != "two" {
+		t.Error("we need two but got : ", text)
+	}
+}
+
+func TestOperandToText_WhenGiven9_ShouldReturnNine(t *testing.T) {
+	text := operandToText(9);
+	if text != "nine" {
+		t.Error("we need nine but got : ", text)
+	}
+}
+
