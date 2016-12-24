8c6d60245bbafec8ac58df80d6525c7be787a90c
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 10:24:56 +0700

    declare slice

--- src/hello/main.go
+++ src/hello/main.go
@@ -9,9 +9,12 @@ func main() {
 	var arr [5]int
 	fmt.Println(arr)
 
+	var sl []int
+	fmt.Println(sl)
+
 	name := "P"
 	age := 1
 	fmt.Println(welcome, "Hello", name)
 	fmt.Println("age", age)
-	sayHi()
+	//sayHi()
 }
