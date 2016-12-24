1490d7d7e2353a752c60c8fcfd30964f7d4cb417
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 10:28:13 +0700

    Add element to Array

--- src/hello/main.go
+++ src/hello/main.go
@@ -7,9 +7,12 @@ const welcome = "Hello"
 func main() {
 
 	var arr [5]int
+	arr[1] = 2
+	arr[2] = 5
+	arr[3] = 8
 	fmt.Println(arr)
 
-	var sl = []int{1,2}
+	var sl = []int{}
 	sl = append(sl,2)
 	fmt.Println(sl)
 
