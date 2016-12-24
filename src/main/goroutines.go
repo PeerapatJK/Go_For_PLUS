0a9dc711029133c6ea8546d3b0d655a9feee18a8
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 15:32:24 +0700

    Add wait group

--- src/main/goroutines.go
+++ src/main/goroutines.go
@@ -3,15 +3,27 @@ package main
 import (
 	"time"
 	"fmt"
+	"strconv"
+	"sync"
 )
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(1000 * time.Millisecond)
-		fmt.Println(s)
+func say(s string, t int) {
+	for i := 1; i <= 5; i++ {
+
+		time.Sleep(time.Duration(1000 * t) * time.Millisecond)
+		fmt.Println(s + "  " + strconv.Itoa(i))
 	}
 }
 func main() {
-	go say("hello")
-	say("world")
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	go say("hello", 1)
+	go say("world", 2)
+	go func() {
+		time.Sleep(2000 * time.Millisecond)
+		fmt.Println("Hello World Start ----------------")
+	}()
+
+	wg.Wait()
 }
