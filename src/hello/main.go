f740553237f0f90a2d50138401c6186681227289
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 10:29:33 +0700

    Print length

--- src/hello/main.go
+++ src/hello/main.go
@@ -11,10 +11,12 @@ func main() {
 	arr[2] = 5
 	arr[3] = 8
 	fmt.Println(arr)
+	fmt.Println("arr length : ", len(arr))
 
 	var sl = []int{}
-	sl = append(sl,2)
+	sl = append(sl, 2)
 	fmt.Println(sl)
+	fmt.Println("sl length : ", len(sl))
 
 	name := "P"
 	age := 1
