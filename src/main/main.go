67753cbde8791d79cee9d1126b7f1441da07a664
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-24 14:27:09 +0700

    Add foreach

--- src/main/main.go
+++ src/main/main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("For")
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
 
-	j := 1;
+	fmt.Println("While")
+	var j int;
 	for j < 10 {
 		fmt.Println(j)
 		j++;
 	}
+
+	fmt.Println("ForEach")
+	ls := []string{"apple","banana","abc"}
+	for k,v:= range ls{
+		fmt.Println(k,v)
+	}
+
 }
